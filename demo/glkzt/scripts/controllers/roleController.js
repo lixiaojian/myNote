f899b5f526be660f935396c5a998ebf60a8a2096
376127890@qq.com <376127890@qq.com>
2015-06-24 17:21:26 +0800

    添加角色管理

---
+++ demo/glkzt/scripts/controllers/roleController.js
@@ -0,0 +1,85 @@
+/**
+ * Created by lixj(872458899@qq.com) on 15/6/24.
+ */
+;(function(app){
+    app.controller('jsglCtr',['$scope','$modal','roleService',function($scope,$modal,roleService){
+        $scope.modileTitle='角色管理';
+        //获取所有角色
+        roleService.getAll({},function(data){
+            $scope.datas = data;
+        });
+
+        //打开弹层
+        $scope.openNew = function(type,id){
+            $modal.open({
+                animation: true,
+                templateUrl: 'myModalContent.html',
+                controller: ['$scope','$modalInstance',function($scope, $modalInstance){
+
+                    $scope.isCheck = false;
+                    if('add' === type){
+                        $scope.title='新增';
+                    }else if('select' === type){
+                        $scope.isCheck = true;
+                        $scope.title='查看';
+                    }else if('edit' === type){
+                        $scope.title='编辑';
+                    }
+                    if(id){
+                        roleService.findById({id:id},function(data){
+                            $scope.role = data;
+                        });
+                    }else{
+                        $scope.role ={
+                            id:'',
+                            roleName:'',
+                            roleDesc:'',
+                            //分配权限的树结构
+                            authoritys:[
+                                {
+                                    moduleName:'操作员管理',
+                                    subModule:[
+                                        {
+                                            subName:'操作员管理',
+                                            select:{name:'查看',has:false},
+                                            add:{name:'新增',has:false},
+                                            edit:{name:'编辑',has:false},
+                                            delete:{name:'删除',has:false}
+                                        },
+                                        {
+                                            subName:'角色管理',
+                                            select:{name:'查看',has:false},
+                                            add:{name:'新增',has:false},
+                                            edit:{name:'编辑',has:false},
+                                            delete:{name:'删除',has:false}
+                                        }
+                                    ]
+                                },
+                                {
+                                    moduleName:'用户管理',
+                                    subModule:[
+                                        {
+                                            subName:'用户信息',
+                                            select:{name:'查看',has:false},
+                                            edit:{name:'编辑',has:false},
+                                            export:{name:'导出',has:false}
+                                        }
+                                    ]
+                                }
+                            ]
+                        };
+                    }
+                    //点击确定
+                    $scope.ok = function () {
+                        $modalInstance.close();
+                        console.log($scope.role);
+                    };
+                    //点击取消
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+                }]
+            });
+        };
+    }]);
+}(angular.module('backStage')));
---
+++ demo/glkzt/scripts/services/roleService.js
@@ -0,0 +1,23 @@
+/**
+ * Created by lixj(872458899@qq.com) on 15/6/22.
+ */
+;(function (app) {
+    app.service('roleService', ['$resource',function ($resource) {
+        var url = '';
+        var actions = {
+            //获取所有角色
+            getAll:{
+                url:'data/roles.json',
+                method:'get',
+                isArray:true
+            },
+            findById:{
+                url:'data/role.json?id=:id',
+                method:'get'
+            }
+        };
+        return $resource(url,{},actions);
+    }]);
+}(angular.module('backStage')));
+
+
