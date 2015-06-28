74d23d11ef35b949f08c7c35a22b887495684ce9
376127890@qq.com <376127890@qq.com>
2015-06-28 09:49:47 +0800

    添加文件上传

---
+++ demo/quark/scripts/directives/commonDirectives.js
@@ -0,0 +1,53 @@
+/**
+ * Created by lixj(872458899@qq.com) on 15/6/27.
+ */
+;(function(app){
+    //文件上传
+    app.directive('inputFile', ['$upload', function ($upload) {
+        return {
+            restrict: 'EA',
+            scope: {
+                title: '@',
+                model: '='
+            },
+            replace: true,
+            template: '\
+                <div class="form-group"> \
+                    <div ng-show="progressing" class="progress progress-striped active">\
+                        <div class="progress-bar bg-color-darken" role="progressbar" style="width: {{progress}}%"></div>\
+                    </div>\
+                    <div ng-show="!progressing" class="input-group">\
+                        <span style="cursor: pointer;" data-ng-file-change="fileChange($files)" ng-file-select>\
+                            <i class="glyphicon glyphicon-upload"></i> 上传{{title}} </span> \
+                    </div> \
+                </div>',
+            link: function linkFun(scope) {
+                // 文件上传实现
+                scope.fileChange = function ($files) {
+                    scope.upload = $upload.upload({
+                        //上传地址
+                        url: 'data/fileUpload.json',
+                        method: 'POST',
+                        withCredentials: true,
+                        data: {},
+                        file: $files
+                    }).progress(function(evt) {
+                        scope.progressing = true;
+                        scope.progress = parseInt(100.0 * evt.loaded / evt.total);
+                    }).success(function(data) {
+                        scope.progressing = false;
+                        if('0000'==data.resCode) {
+                            for(var i=0; i<data.resInfo.length; i++) {
+                                scope.model = data.resInfo[i].savePath?data.resInfo[i].savePath:'';
+                            }
+                        } else {
+                            console.log('上传失败')
+                        }
+                    }).error(function () {
+                        scope.progressing = false;
+                    });
+                };
+            }
+        }
+    }])
+}(angular.module('backStage')));
