4ac2c1673735de2b39f8365e3844a4e668c7eb00
376127890@qq.com <376127890@qq.com>
2015-07-04 07:33:12 +0800

    绑卡信息

--- demo/quark/scripts/controllers/userController.js
+++ demo/quark/scripts/controllers/userController.js
@@ -4,6 +4,35 @@
 ;(function(app){
     app.controller('userCtr',['$scope','$modal',function($scope,$modal){
         $scope.modileTitle1 = '用户信息';
+        //日历相关
+        $scope.bopen = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.bopened = !$scope.bopened;
+        };
+        $scope.eopen = function($event) {
+            $event.preventDefault();
+            $event.stopPropagation();
+            $scope.eopened = !$scope.eopened;
+        };
+        $scope.format = 'yyyy-MM-dd';
+        //分页
+        $scope.page = {
+            //总记录数
+            totalItems:120,
+            //当前页 default:1
+            curPage:1,
+            //每页条数 default:10
+            itemsPerPage:3,
+            //显示页数
+            maxSize:10,
+            previousText:'上一页',
+            nextText:'下一页',
+            //是否显示首页和末页
+            boundaryLinks:true,
+            firstText:'首页',
+            lastText:'末页'
+        };
         $scope.modal = function(){
             var modalInstance = $modal.open({
                 animation: true,
