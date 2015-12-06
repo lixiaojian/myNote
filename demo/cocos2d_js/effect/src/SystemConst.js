bd7f7178aa2afdcfa9c8a64fd0155e1381f2e573
lxj <872458899@qq.com>
2015-12-06 09:53:49 +0800

    3d效果

---
+++ demo/cocos2d_js/effect/src/SystemConst.js
@@ -0,0 +1,16 @@
+/**
+ * Created by 872458899@qq.com on 15/12/5.
+ */
+//操作标识
+var ActionTypes = {
+    PAGEJUIN3D:101,
+    LENS3D:102,
+    SHAKY3D:103,
+    WAVES3D:104,
+    JUMPJILES3D:105,
+    SHAKYJILES3D:106,
+    WAVESJILES3D:107,
+    FLIPX3D:108
+};
+
+var SP_TAG = 1000;
---
+++ demo/cocos2d_js/effect/src/resource.js
@@ -0,0 +1,18 @@
+/**
+ * Created by 872458899@qq.com on 15/12/5.
+ */
+var res = {
+    'backdown_png':'res/Back-down.png',
+    'backup_png':'res/Back-up.png',
+    'background_png':'res/Background.png',
+    'godown_png':'res/Go-down.png',
+    'goup_png':'res/Go-up.png',
+    'hero_png':'res/hero.png',
+    'fnt2_png':'res/fonts/fnt2.png',
+    'fnt2_fnt' : "res/fonts/fnt2.fnt"
+};
+
+var g_res=[];
+for(var i in res){
+    g_res.push((res[i]));
+}
