e8a0bcbb60725bf7b5e1c825f053f9c2d67ee185
xiaojianli <376127890@qq.com>
2017-04-18 11:50:51 +0800

    添加关于我的页面

--- app/full-page/scripts/index.js
+++ app/full-page/scripts/index.js
@@ -46,7 +46,7 @@ class MainPage extends React.Component{
         return(
             <div>
                 <div id="myContainer">
-                    <div className="first-section section" id="sss1">
+                    <div className="first-section section">
                         <div className="intro">
                             <div><img className="user-header-img" src={userHeaderImg} alt=""/></div>
                             <h2><div className="my-motto">按照自己的意志去做，不要听那些闲言碎语，你就一定会成功。</div></h2>
@@ -55,17 +55,56 @@ class MainPage extends React.Component{
                             <h2><span className="user-name">李晓健</span></h2>
                         </div>
                     </div>
-                    <div className="second-section section">
-                        <div className="intro">
-                            <h1>关于我</h1>
-                            <div className="about-me-warpper">
-                                <ul className="about-me-box clearfix">
-                                    <li className="about-me-item"><span className="icon-education"></span>年龄</li>
-                                    <li className="about-me-item"><span className="icon-state"></span>学历</li>
-                                    <li className="about-me-item">位置</li>
-                                    <li className="about-me-item">状态</li>
-                                </ul>
+                    <div className="second-section section about-me">
+                        <h2>关于我</h2>
+                        <div className="about-me-warpper">
+                            <ul className="about-me-box clearfix">
+                                <li className="about-me-item my-birthday">
+                                    <div className="about-me-icon">
+                                        <div className="spinner"></div>
+                                        <span className="icon-cake"></span>
+                                        <span className="text-icon-desc">生日</span>
+                                    </div>
+                                    <div className="about-me-value">1987-02-04</div>
+                                </li>
+                                <li className="about-me-item my-education">
+                                    <div className="about-me-icon">
+                                        <div className="spinner"></div>
+                                        <span className="icon-education"></span>
+                                        <span className="text-icon-desc">学历</span>
+                                    </div>
+                                    <div className="about-me-value">大专</div>
+                                </li>
+                                <li className="about-me-item my-position">
+                                    <div className="about-me-icon">
+                                        <div className="spinner"></div>
+                                        <span className="icon-position"></span>
+                                        <span className="text-icon-desc">位置</span>
+                                    </div>
+                                    <div className="about-me-value">上海浦东</div>
+                                </li>
+                                <li className="about-me-item my-state">
+                                    <div className="about-me-icon">
+                                        <div className="spinner"></div>
+                                        <span className="icon-state"></span>
+                                        <span className="text-icon-desc">状态</span>
+                                    </div>
+                                    <div className="about-me-value">在职</div>
+                                </li>
+                            </ul>
+                            <div className="about-me-text">
+                                <p>六年互联网经验,三年半全职前端开发经验</p>
+                                <p>自学能力强，喜欢钻研新技术，敢于面对和克服困难</p>
+                                <p>具有很强的团队精神，有良好的组织、协调和沟通能力，有强烈的集体荣誉感</p>
+                                <p>有比较强的动手能力，勇于面对困难和挑战，有很好的分析问题与解决问题的能力</p>
+                                <p>本人热爱软件事业，对IT领域的软件开发和设计工作有浓厚的兴趣，能承受较大的工作压力</p>
                             </div>
+                        </div>
+                    </div>
+                    <div className="fourth-section section">
+                        <div className="intro">
+                            <h1>IMPRESS</h1>
+                            <p>Take it to the next level!</p>
                         </div>
                     </div>
                     <div className="third-section section">
@@ -93,12 +132,6 @@ class MainPage extends React.Component{
                                 <h1>DOCUMENTED</h1>
                                 <p>If fullPage.js is known for one thing it's for its great documentation!</p>
                             </div>
-                        </div>
-                    </div>
-                    <div className="fourth-section section">
-                        <div className="intro">
-                            <h1>IMPRESS</h1>
-                            <p>Take it to the next level!</p>
                         </div>
                     </div>
                 </div>
