3d9ef9feda302253fef941993f64ee8023ef0724
xiaojianli <376127890@qq.com>
2017-04-26 17:59:18 +0800

    添加工作经历模块

--- app/resume2/scripts/components/WorkExperience.js
+++ app/resume2/scripts/components/WorkExperience.js
@@ -3,15 +3,33 @@
  */
 import React from 'react';
 import ModuleTitle from '../components/ModuleTitle';
+import workExp from '../../../../mockData/wordExperience.json';
 import '../../styles/workExperience.less';
 
 export default class WorkExperience extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            workExp:workExp.datas
+        }
+    }
+
     render(){
         return(
             <div className="work-experience-warpper modeule-box" id="work_experience">
                 <ModuleTitle radius={true} title="工作经验"/>
-                <ul>
-                    <li>12321312321</li>
+                <ul className="work-experience-box">
+                    {this.state.workExp.map((item,index) => {
+                        return(
+                            <li key={index} className="work-experience-item">
+                                <div className="item-header">
+                                    <span>{item.jobTitle}</span>
+                                    <span className="time-warrper">{item.date}</span>
+                                </div>
+                                <p>{item.workDesc}</p>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         )
