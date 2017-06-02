cf680c57589ab26758125ce870d5a47e6951d106
xiaojianli <376127890@qq.com>
2017-06-02 14:11:50 +0800

    处理seo

--- demo/mvvm-master/src/Mvvm.js
+++ demo/mvvm-master/src/Mvvm.js
@@ -6,28 +6,10 @@ class MVVM{
     constructor(config={}){
         this.config = config
         this._data = config.data;
-        // 数据代理
-        // 实现 vm.xxx -> vm._data.xxx
-        Object.keys(this._data).forEach( key => {
-            this._proxyData(key);
-        });
-
         observe(this._data);
         this.$compile = new Compile(config.el || document.body, this)
     }
     $watch(key,cb){
         new Watcher(this, key, cb);
-    }
-    _proxyData(key){
-        Object.defineProperty(this, key, {
-            configurable: false,
-            enumerable: true,
-            get: () => {
-                return this._data[key];
-            },
-            set: (newVal) => {
-                this._data[key] = newVal;
-            }
-        });
     }
 }
